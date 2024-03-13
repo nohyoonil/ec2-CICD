8b29ab9601a1ba78ea5c35059c5a3bc2df99a1f3
yuun-dev <yuun.dev@gmail.com>
2024-03-13 12:53:36 +0900

    first commit

---
+++ src/main/java/com/yoon/ec2CICD/Ec2CicdApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.ec2CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2CicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoon/ec2CICD/Ec2CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.ec2CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
